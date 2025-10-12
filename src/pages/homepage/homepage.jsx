2f847be70ee0551a9ba3f62a10780f475008abb9
aarabhamid <hamidaarab.pro@gmail.com>
2025-10-12 21:28:24 +0200

    slider fonctionnel

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,18 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import instanceAxios from "../../utils/axios";
 import MovieCard from "../../components/movieCard/movieCard";
+import PersonCard from "../../components/personCard/personcard";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./homepage.css";
+import { GrNext, GrPrevious } from "react-icons/gr";
 
-function SliderComponent () {
+function SliderComponent() {
   const [movies, setMovies] = useState([]);
   const [tvShows, setTvShows] = useState([]);
+  const [person, setPerson] = useState([]);
+  const movieSliderRef = useRef(null);
+  const tvSliderRef = useRef(null);
+  const personSliderRef = useRef(null);
 
   const fetchData = async () => {
     try {
       const response = await instanceAxios.get("/movies");
       const tvResponse = await instanceAxios.get("/tv");
+      const personResponse = await instanceAxios.get("/person");
+      setPerson(personResponse.data.results);
       setTvShows(tvResponse.data.results);
       setMovies(response.data.results);
     } catch (error) {
@@ -25,46 +34,59 @@ function SliderComponent () {
   }, []);
 
   const settings = {
-    dots: false, // Désactive les points en bas
+    dots: false,
     infinite: true,
-    speed: 500,
+    speed: 2000,
     slidesToShow: 10,
-    slidesToScroll: 1,
-    arrows: true, // Active les flèches
-    nextArrow: <SampleNextArrow />, // Composant personnalisé pour la flèche suivante
-    prevArrow: <SamplePrevArrow />, // Composant personnalisé pour la flèche précédente
+    slidesToScroll: 10,
+    arrows: false, // Désactive les flèches par défaut du slider
     responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 7,
+          slidesToScroll: 7,
+          infinite: true,
+        },
+      },
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 6,
-          slidesToScroll: 3,
+          slidesToShow: 5,
+          slidesToScroll: 5,
           infinite: true,
-        }
+        },
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 2,
+        },
+      },
+    ],
   };
 
   return (
-    <div>
-      <h2>Les films de la semaine</h2>
-      <div style={{ padding: '0 50px' }}>
-        <Slider {...settings}>
+    <div className="section-slider">
+
+      <div className="title-and-arrows">
+        <h2>Les films de la semaine</h2>
+        <div className="arrows">
+          <GrPrevious onClick={() => movieSliderRef.current.slickPrev()} />
+          <GrNext onClick={() => movieSliderRef.current.slickNext()} />
+        </div>
+      </div>
+      <div style={{ padding: "0 50px" }}>
+        <Slider ref={movieSliderRef} {...settings}>
           {movies.map((movie) => (
             <div key={movie.id}>
               <MovieCard movie={movie} />
@@ -72,9 +94,16 @@ function SliderComponent () {
           ))}
         </Slider>
       </div>
-      <h2>Les séries de la semaine</h2>
-      <div style={{ padding: '0 50px' }}>
-        <Slider {...settings}>
+
+      <div className="title-and-arrows">
+        <h2>Les séries de la semaine</h2>
+        <div className="arrows">
+          <GrPrevious onClick={() => tvSliderRef.current.slickPrev()} />
+          <GrNext onClick={() => tvSliderRef.current.slickNext()} />
+        </div>
+      </div>
+      <div style={{ padding: "0 50px" }}>
+        <Slider ref={tvSliderRef} {...settings}>
           {tvShows.map((show) => (
             <div key={show.id}>
               <MovieCard movie={show} />
@@ -82,31 +111,25 @@ function SliderComponent () {
           ))}
         </Slider>
       </div>
+
+      <div className="title-and-arrows">
+        <h2>Les artistes de la semaine</h2>
+        <div className="arrows">
+          <GrPrevious onClick={() => personSliderRef.current.slickPrev()} />
+          <GrNext onClick={() => personSliderRef.current.slickNext()} />
+        </div>
+      </div>
+      <div style={{ padding: "0 50px" }}>
+        <Slider ref={personSliderRef} {...settings}>
+          {person.map((person) => (
+            <div key={person.id}>
+              <PersonCard person={person} />
+            </div>
+          ))}
+        </Slider>
+      </div>
+
     </div>
-  );
-}
-
-// Composant pour la flèche suivante
-function SampleNextArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", alignItems: "center", background: "black", borderRadius: "50%" }}
-      onClick={onClick}
-    />
-  );
-}
-
-// Composant pour la flèche précédente
-function SamplePrevArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "black", borderRadius: "50%" }}
-      onClick={onClick}
-    />
   );
 }
 

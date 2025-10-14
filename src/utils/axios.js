3e8a7ab8b045567fcb34dcd6c67c78e964bd8c45
aarabhamid <hamidaarab.pro@gmail.com>
2025-10-14 21:24:53 +0200

    correction url axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const instanceAxios = axios.create({
-    baseURL: "http://localhost:3000", // URL de votre API
+     baseURL: import.meta.env.VITE_API_URL, // URL de votre API
     //withCredentials: true, // Inclut les cookies
   });
   
+  console.log("URL du backend :", import.meta.env.VITE_API_URL);
+
   export default instanceAxios;
